9af8e15787c2f9dd45cbc348a124c8e31206e3b9
Mateusz <holowieszko.m@gmail.com>
2019-02-21 12:36:37 +0100

    Added types for request object

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,15 @@
 import { Controller, Post, Body } from '@nestjs/common';
+import { UserDTO } from './user.dto';
 
 @Controller('api/users')
 export class UserController {
   @Post('login')
-  login(@Body() data) {
-    return 'login';
+  login(@Body() data: UserDTO) {
+    return data;
   }
 
   @Post('register')
-  register(@Body() data) {
-    return 'register';
+  register(@Body() data: UserDTO) {
+    return data;
   }
 }
