e2b5e8c7ac0b2499ceebabf4f2aed62cea25024e
Mateusz <holowieszko.m@gmail.com>
2019-02-21 19:02:46 +0100

    Added Auth Guard for whole notes controller

--- src/note/note.module.ts
+++ src/note/note.module.ts
@@ -1,16 +1,22 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { NoteService } from './note.service';
 import { NoteController } from './note.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { NoteEntity } from './note.entity';
 import { UserEntity } from 'src/user/user.entity';
+import { AuthGuard } from 'src/common/auth.guard';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([NoteEntity, UserEntity])
   ],
   providers: [
-    NoteService
+    NoteService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard
+    }
   ],
   controllers: [NoteController]
 })
