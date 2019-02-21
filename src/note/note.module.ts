fe2988bbb1cb9392d6f36aec077c7bc4a108c269
Mateusz <holowieszko.m@gmail.com>
2019-02-21 13:53:52 +0100

    Deleted AuthGuard from providers, moved to AppModule

--- src/note/note.module.ts
+++ src/note/note.module.ts
@@ -3,12 +3,16 @@ import { NoteService } from './note.service';
 import { NoteController } from './note.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { NoteEntity } from './note.entity';
+import { AuthGuard } from 'src/common/auth.guard';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([NoteEntity])
   ],
-  providers: [NoteService],
+  providers: [
+    NoteService,
+    // AuthGuard
+  ],
   controllers: [NoteController]
 })
 export class NoteModule {}
