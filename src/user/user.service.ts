7de2c1f25f46ef5fcb21baccda98cd4d7807d009
Mateusz <holowieszko.m@gmail.com>
2019-02-21 12:41:25 +0100

    Added method for user registration

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserEntity } from './user.entity';
 import { Repository } from 'typeorm';
+import { UserDTO } from './user.dto';
 
 @Injectable()
 export class UserService {
@@ -10,4 +11,17 @@ export class UserService {
     private readonly userRepository: Repository<UserEntity>
   ) {}
 
+  async register(data: UserDTO): Promise<any> {
+    const { username } = data;
+    let user = await this.userRepository.findOne({ where: { username }});
+
+    if (user) {
+      // return 'This users exists!';
+      throw new HttpException('This user exists!', HttpStatus.BAD_REQUEST);
+    }
+
+    user = await this.userRepository.save(data);
+
+    return user;
+  }
 }
