6e63fe736aee0ead7485c5a588199ff4bb349286
Mateusz <holowieszko.m@gmail.com>
2019-02-21 14:13:38 +0100

    Added created, updated column & method for returning data on response

--- src/note/note.entity.ts
+++ src/note/note.entity.ts
@@ -1,13 +1,30 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, CreateDateColumn, UpdateDateColumn } from "typeorm";
+import { UserEntity } from "src/user/user.entity";
 
 @Entity('notes')
 export class NoteEntity {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @CreateDateColumn()
+  created: Date;
+
+  @UpdateDateColumn()
+  updated: Date;
+
   @Column()
   title: string;
 
   @Column('text')
   description: string;
+
+  @ManyToOne(type => UserEntity, author => author.notes)
+  author: UserEntity;
+
+  toResponseObject() {
+    const { id, title, description, created, updated } = this;
+    const responseObject = { id, title, description, created, updated };
+
+    return responseObject;
+  }
 }
