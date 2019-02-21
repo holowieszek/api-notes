7549339522da0b695bc4dd9608729ff0162e025d
Mateusz <holowieszko.m@gmail.com>
2019-02-21 13:44:13 +0100

    Added Note module to imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,11 +6,13 @@ import { LoggingInterceptor } from './common/logging.interceptor';
 import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { HttpErrorFilter } from './common/http-error.fitler';
+import { NoteModule } from './note/note.module';
 
 @Module({
   imports: [
     UserModule,
-    TypeOrmModule.forRoot()
+    TypeOrmModule.forRoot(),
+    NoteModule
   ],
   controllers: [AppController],
   providers: [
