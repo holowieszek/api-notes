e86ff52047533c5e703659339ac80d9b9d91087c
Mateusz <holowieszko.m@gmail.com>
2019-02-21 14:11:45 +0100

    Added method for creating notes

--- src/note/note.service.ts
+++ src/note/note.service.ts
@@ -2,15 +2,23 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { NoteEntity } from './note.entity';
 import { Repository } from 'typeorm';
+import { NoteDTO } from './note.dto';
+import { UserEntity } from 'src/user/user.entity';
 
 @Injectable()
 export class NoteService {
   constructor(
     @InjectRepository(NoteEntity)
-    private readonly noteRepository: Repository<NoteEntity>
+    private readonly noteRepository: Repository<NoteEntity>,
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>
   ) {}
   
-  create(data) {
-    return 'create note';
+  async create(userId: string, data: NoteDTO) {
+    const user = await this.userRepository.findOne({ where: { id: userId }});
+    const note = await this.noteRepository.create({ ...data, author: user });
+
+    await this.noteRepository.save(note);
+    return note.toResponseObject();
   }
 }
