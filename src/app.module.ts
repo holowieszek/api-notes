d5cad064dcb966a4b5a4d0b87e1f9a52d7fb10a3
Mateusz <holowieszko.m@gmail.com>
2019-02-21 11:46:23 +0100

    Added User Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { LoggingInterceptor } from './common/logging.interceptor';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule],
   controllers: [AppController],
   providers: [
     AppService,
