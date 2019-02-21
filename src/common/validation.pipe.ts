ac678daed5261e3f103178bf27c21d9ae791532a
Mateusz <holowieszko.m@gmail.com>
2019-02-21 20:41:55 +0100

    Added validation pipe with custom error response

---
+++ src/common/validation.pipe.ts
@@ -0,0 +1,54 @@
+import { PipeTransform, Injectable, ArgumentMetadata, BadRequestException, HttpException, HttpStatus } from '@nestjs/common';
+import { validate } from 'class-validator';
+import { plainToClass } from 'class-transformer';
+
+@Injectable()
+export class ValidationPipe implements PipeTransform<any> {
+  async transform(value, { metatype }: ArgumentMetadata) {
+
+    if (value instanceof Object && this.isEmpty(value)) {
+      throw new HttpException('Validation failed: No body submitted', HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+    
+    if (!metatype || !this.toValidate(metatype)) {
+      return value;
+    }
+
+    const object = plainToClass(metatype, value);
+    const errors = await validate(object);
+
+    const parsedErrors = this.formatErrors(errors);
+
+    if (errors.length > 0) {
+      // throw new HttpException(`Validation failed: ${this.formatErrors(errors)}`, HttpStatus.UNPROCESSABLE_ENTITY);
+      throw new HttpException(parsedErrors, 200);
+    }
+    return value;
+  }
+
+  private toValidate(metatype): boolean {
+    const types = [String, Boolean, Number, Array, Object];
+    return !types.find((type) => metatype === type);
+  }
+
+  private formatErrors(errors: any[]) {
+    const parsedErrors: any = []
+    errors.map((error, index) => {
+      for (let property in error.constraints) {
+        parsedErrors.push({
+            [error.property]: error.constraints[property]
+          });
+      }
+    });
+    
+    return parsedErrors;
+  }
+
+  private isEmpty(value: any) {
+    if (Object.keys(value).length > 0) {
+      return false;
+    }
+
+    return true;
+  }
+}
