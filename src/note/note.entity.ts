2f1af9e91ecf8622f3055cd17100a98002795631
Mateusz <holowieszko.m@gmail.com>
2019-03-04 11:18:14 +0100

    Added relation to note entity

--- src/note/note.entity.ts
+++ src/note/note.entity.ts
@@ -1,5 +1,6 @@
-import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, CreateDateColumn, UpdateDateColumn } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, CreateDateColumn, UpdateDateColumn, OneToMany } from "typeorm";
 import { UserEntity } from "src/user/user.entity";
+import { FileEntity } from "src/upload/upload.entity";
 
 @Entity('notes')
 export class NoteEntity {
@@ -20,6 +21,9 @@ export class NoteEntity {
 
   @ManyToOne(type => UserEntity, author => author.notes)
   author: UserEntity;
+
+  @OneToMany(type => FileEntity, file => file.id)
+  files: FileEntity[];
 
   toResponseObject() {
     const { id, title, description, created, updated } = this;
